20be41c34d759b82277fffc526d8592b7e2b1bfd
A. Vatsaev <azero79@gmail.com>
2014-12-20 11:25:13 +0100

    Initial Commit

---
+++ CueDramaticHorn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CueDramaticHorn
+//
+//  Created by Aslan Vatsaev on 20/12/14.
+//  Copyright (c) 2014 Aslan Vatsaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
